319c9381bae4d110aa2ad39caeb7befc7a75841f
gtg092x <mdrake@mediadrake.com>
2014-05-15 10:31:12 -0400

    fixed root support

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var WP = require("../index");
-
-WP.load({path:"/Users/matthew.drake/dev/wp-test"},function(WP){	
-		
-	WP.scaffold.plugin("my-plugin",{plugin_name:"Hello Node WP"},function(err,result){ // creates a new plugin
-		console.log(result);
-	});	
-	
-});
